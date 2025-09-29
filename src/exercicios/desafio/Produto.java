edc09d283673594e61e3c3474f524b879fd2fe4c
lougans meneses <lougans_meneses@hotmail.com>
2025-09-29 16:52:49 -0300

    desafio para entrega realizado

---
+++ src/exercicios/desafio/Produto.java
@@ -0,0 +1,34 @@
+package exercicios.desafio;
+
+public class Produto {
+    private String nome;
+    private double preco;
+    private String categoria;
+
+    public Produto(String nome, double preco, String categoria) {
+        this.nome = nome;
+        this.preco = preco;
+        this.categoria = categoria;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "nome='" + nome + '\'' +
+                ", preco=" + preco +
+                ", categoria='" + categoria + '\'' +
+                '}';
+    }
+}
--- src/exercicios/exercicioEmSala/Main.java
+++ src/exercicios/exercicioEmSala/Main.java
@@ -1,13 +1,12 @@
-package exercicio;
+package exercicios.exercicioEmSala;
 
-import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static List<Produto> paginar(List<Produto> lista, int pagina, int tamanho){
-
-    }
+//    public static List<Produto> paginar(List<Produto> lista, int pagina, int tamanho){
+//
+//    }
     public static void main(String[] args) {
         List<Produto> produtos = new ArrayList<>();
         for (int i = 1; i <= 10; i++) {
