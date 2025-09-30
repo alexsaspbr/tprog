763f61bf5c17ca1853bcc498cff8d257904938dd
Alex Araujo <alexsaspbr@gmail.com>
2025-09-29 22:14:27 -0300

    functional interfaces

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,47 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Scanner;
+import java.time.LocalDate;
+import java.util.function.*;
 
 public class Main {
     public static void main(String[] args) {
 
-        //IO
-        File file = new File("/home/alexaraujo/ada/turmas/1418/tprog/src/resources/nomes.txt");
-        System.out.printf("Arquivo existe %b", file.exists());
+        //functional interfaces
 
-        try {
-            Scanner input = new Scanner(file);
-            while (input.hasNext()) {
-                System.out.println(input.nextLine());
-            }
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
+        //supplier
+        Supplier<LocalDate> sv = () -> LocalDate.now();
+        System.out.println(sv.get());
 
-        //NIO
-        Path path = Path.of("/home/alexaraujo/ada/turmas/1418/tprog/src/resources/nomes.txt");
-        Path path2 = Paths.get("home", "alexaraujo", "ada", "turmas", "1418", "tprog", "src", "resources", "nomes.txt");
+        //predicate
+        Predicate<Integer> isNegative = (Integer a) -> a < 0;
+        System.out.println(isNegative.test(-1));
 
-        System.out.println(file.getParent());
-        System.out.println(file.isAbsolute());
-        String newFile = file.getParent().concat(File.separator).concat("turma1418.txt");
-        File novoArquivo = new File(newFile);
-        try {
-            novoArquivo.createNewFile();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        //bi-predicate
+        BiPredicate<Integer, Integer> bi_predicate = (Integer a, Integer b) -> a < 0 && b < 0;
+        System.out.println(bi_predicate.test(-1, 9));
+
+        //consumer
+        Consumer<String> consumer = (String texto) -> System.out.println(texto);
+        consumer.accept("Alex Araujo");
+
+        //bi-consumer
+        BiConsumer<String, String> bi_consumer = (String nome, String sobreNome) -> System.out.printf("\n%s %s\n", nome, sobreNome);
+        bi_consumer.accept("Joao", "Carlos");
+
+        //function
+        Function<String, Integer> function = (String texto) -> texto.length();
+        System.out.println(function.apply("Ola mundo!"));
+
+        //bi-function
+        BiFunction<String, String, Integer> bi_function = (String texto, String texto2) -> texto.length() + texto2.length();
+        System.out.println(bi_function.apply("Ola", "Mundo!"));
+
+        //unaryOperator
+        UnaryOperator<String> unaryOperator = (String nome) -> nome.concat("!");
+        System.out.println(unaryOperator.apply("Atencao"));
+
+        //binaryOperator
+         BinaryOperator<String> binaryOperator = (String nome, String sobreNome) -> nome.concat(" ").concat(sobreNome);
+         System.out.println(binaryOperator.apply("Guilherme", "Ariel"));
+
 
     }
 }
---
+++ src/Veiculo.java
@@ -0,0 +1,10 @@
+public class Veiculo {
+
+    private String placa;
+    private String marca;
+
+    public Veiculo(String placa, String marca) {
+        this.placa = placa;
+        this.marca = marca;
+    }
+}
